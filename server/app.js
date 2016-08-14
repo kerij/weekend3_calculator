66e0e3fff788b7a7880da2a4d1adc3a56e09002f
kerij <keriajackson@gmail.com>
2016-08-13 20:43:53 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+
+
+//any request to a url
+app.get('/*', function(req, res) {
+    var file = req.params[0] || '/views/index.html';
+    res.sendFile(path.join(__dirname, './public', file));
+    console.log(req.params[0]); //concatonates strings, cross-browser performance
+});
+
+//set our port
+app.set('port', process.env.PORT || 3000);
+
+//listen
+app.listen(app.get('port'), function() {
+    console.log("The server is running on port ", app.get('port')); //this is nice to have
+});
